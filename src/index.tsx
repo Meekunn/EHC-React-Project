52fa85fbd5c441ea63357ed86e657631fe9c32e9
Meekun <orofinadedamola@gmail.com>
2022-08-28 23:23:02 +0100

    :label: Fix TypeError

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.scss'
-import App from './App'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
+import App from './App'
+import './index.scss'
 
-const container = document.getElementById('root');
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+const container = document.getElementById('root')
+const root = createRoot(container!)
 root.render(  
   <StrictMode>
-    <BrowserRouter>
+      <BrowserRouter>
         <App  />
       </BrowserRouter>
   </StrictMode>
@@ -17,4 +17,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
