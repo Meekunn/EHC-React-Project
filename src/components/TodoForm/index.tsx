a37ff8abccb5b21bb1963ced9c47923b591b1f9a
Meekun <orofinadedamola@gmail.com>
2022-08-29 04:15:30 +0100

    :sparkles: Add useRef to TodoForm

--- src/components/TodoForm/index.tsx
+++ src/components/TodoForm/index.tsx
@@ -1,11 +1,20 @@
+import { useRef, useEffect } from 'react'
 import { HiPlusSm } from 'react-icons/hi'
 
 const TodoForm = ({addTodo, todo, setTodo}: ITodoForm) => {
+
+    const formRef = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        formRef.current?.focus();
+    }, [])
+
     return (
         <form className="todo-form">
             <button type='submit' className='add-btn' onClick={addTodo}><HiPlusSm /></button>
             <input 
-                type='text' 
+                type='text'
+                ref={formRef} 
                 className='input' 
                 placeholder="Add a task"
                 value={todo}
