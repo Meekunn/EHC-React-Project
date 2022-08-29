83687b7bfacb624437af161ae8956f83a52235ca
Meekun <orofinadedamola@gmail.com>
2022-08-29 13:28:25 +0100

    :sparkles: Prevent WhiteSpace In TodoForm From Getting sent to Firestore

--- src/hooks/useAddTodo.ts
+++ src/hooks/useAddTodo.ts
@@ -4,13 +4,13 @@ import { UserAuth } from "../HOC/AuthContext"
 
 const useAddTodo = () => {
 
-    const { userUid } = UserAuth()
+    const { userUid } = UserAuth()    
 
     const add = async (todo: string, collectionName: string)=> {
         if(todo !== '') {
             const collectionRef = collection(db, `${collectionName}/${userUid}/todoList`)
             const payload = {
-                todo: todo,
+                todo: todo.trim(),
                 //creates a timestamp which is unique so we use this as the key when returning documents in the subcollection.
                 time: serverTimestamp(),
                 complete: false
