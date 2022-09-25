6366c057f18ef169046e7388c49cb73f170c55be
Meekun <orofinadedamola@gmail.com>
2022-09-25 09:57:10 +0100

    :lipstick: Validate Input and Add Responsiveness

--- src/components/Dashboard/EditCollectionName.tsx
+++ src/components/Dashboard/EditCollectionName.tsx
@@ -1,34 +1,53 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useCallback, useRef, useEffect } from "react"
+import { FiCheckCircle } from "react-icons/fi"
+import { TiTimes } from "react-icons/ti"
 import "./editCollectionName.scss"
 
-const NAME_REGEX = /^[A-Za-z-]*$/
+const NAME_REGEX = /^[A-Z][a-z-]{3,12}$/
 
 const UpdateCollectionName = () => {
 	const inputRef = useRef<HTMLInputElement>(null)
 	const [collectionName, setCollectionName] = useState("")
+	const [validCollectionName, setValidCollectionName] = useState(false)
 	const [isOpen, setIsOpen] = useState(true)
 
 	useEffect(() => {
 		inputRef.current?.focus()
 	}, [])
 
-	const trimText = useCallback((text: string) => text.replace(/\s+/g, ""), [])
+	useEffect(() => {
+		setValidCollectionName(NAME_REGEX.test(collectionName))
+	}, [collectionName])
 
-	const capitalizeText = (text: string): string => {
-		return text.charAt(0).toUpperCase + text.substring(1).toLowerCase()
-	}
+	const trimText = useCallback((text: string) => text.replace(/\s+/g, ""), [])
 
 	const handleSubmit = (e: any) => {
 		e.preventDefault()
-		console.log(collectionName)
-		setCollectionName("")
+		if (validCollectionName) {
+			console.log(collectionName)
+			setCollectionName("")
+		}
 	}
 
 	return (
 		<div className={isOpen ? "update-form-wrapper open" : "update-form-wrapper"}>
 			<form id="collection_name_form" onSubmit={handleSubmit}>
-				<label htmlFor="collection_name">New Collection Name: </label>
+				<label htmlFor="collection_name">
+					New Collection Name:
+					<FiCheckCircle className={validCollectionName ? "valid" : "hide"} />
+					<TiTimes
+						className={validCollectionName || !collectionName ? "hide" : "invalid"}
+					/>
+				</label>
+				{validCollectionName || !collectionName ? (
+					<></>
+				) : (
+					<p>
+						Length is 4 to 12 Characters and ensure first character is in Uppercase.
+						Special Character allows is <strong>-</strong>
+					</p>
+				)}
 				<input
 					id="collection_name"
 					ref={inputRef}
